010d2f2fa3367785f0ca0e8b884411346b545284
Chris Hayen <chayen@gmail.com>
2015-03-04 13:58:25 -0500

    Messing with layouts and animation speed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  console.log('\'Allo \'Allo!');
+  $('.logo-carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    centerMode: true,
+    easing: 'easeOutBack',
+    edgeFriction: 0.5,
+    useCSS: false,
+    speed: 1000
+  });
+
+});
