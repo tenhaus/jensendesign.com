ab719ff0a0adcafe1c30e5542bfdc168dd72d720
Chris Hayen <chayen@gmail.com>
2015-03-09 15:09:49 -0400

    Added videos to fancybox

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    autoWidth: true,
-    center: true,
-    items: 1,
-    autoplay: true
+  // $(".owl-carousel").owlCarousel({
+  //   loop: true,
+  //   autoWidth: true,
+  //   center: true,
+  //   items: 1,
+  //   autoplay: true
+  // });
+  $(".fancybox").fancybox({
+    helpers: {
+      media: {}
+    }
   });
 });
