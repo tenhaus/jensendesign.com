35eba7cf18d5c6130e1b529f25c74add5726c450
Chris Hayen <chayen@gmail.com>
2015-03-16 14:48:52 -0400

    Updated print

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,22 @@ $(document).ready(function() {
     }
   });
 });
+
+function initialize() {
+  var point = new google.maps.LatLng(39.324422, -76.629472);
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: point,
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: point,
+    map: map
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
