1fabfc3035ca76dc552a79eb806c442c34bb5b68
Chris Hayen <chayen@gmail.com>
2015-04-14 16:54:41 -0400

    Added contact form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,51 @@
 'use strict';
 
 $(document).ready(function() {
-  $('.fancybox').fancybox({
-    helpers: {
-      media: {}
-    }
+  $("#emailsubmit").click(function() {
+    var email = $('#email');
+    sendEmail(email.val());
+
+    // if(email.val() === '' || !re.test(email.val())) {
+    //   $('.email-error').show();
+    //   return false;
+    // }
+    // else {
+    //   sendEmail(email.val());
+    // }
   });
 });
 
-function initialize() {
-  var point = new google.maps.LatLng(39.324422, -76.629472);
-  var mapCanvas = document.getElementById('map-canvas');
-  var mapOptions = {
-    center: point,
-    zoom: 14,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+function sendEmail(toemail) {
+  ga('send', 'event', 'email', 'send', toemail);
 
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: point,
-    map: map
-    });
+  $.ajax({
+    type: "POST",
+    url: "https://mandrillapp.com/api/1.0/messages/send.json",
+    data: {
+      'key': '9F0a1ZxEVOhJw6GLYEZzZw',
+      'message': {
+        'from_email': 'info@jensendesignstudio.com',
+        'to': [
+            {
+              'email': 'chayen@gmail.com',
+              'name': 'Chris Hayen',
+              'type': 'to'
+            },
+            {
+              'email': 'rrytter@jensendesignstudio.com',
+              'name': 'Robert Rytter',
+              'type': 'to'
+            }
+          ],
+        'autotext': 'true',
+        'subject': 'Information Requested from jensendesignstudio.com',
+        'html': toemail
+      }
+    }
+   }).done(function(response) {
+     $('.email-header').text('We will be in touch');
+     $('.email-thanks').toggle('show');
+     $('.info').toggle('hide');
+     $('.form').toggle('hide');
+   });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
