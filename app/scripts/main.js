9b1ee3ca3813c759b3e897c08fb2f30c3b0d5614
Joe Widener <jwidener08@gmail.com>
2016-06-09 16:39:20 -0400

    added edu logo carosuel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,54 @@
 'use strict';
 
+var client = contentful.createClient({
+    // ID of Space
+    space: 'w6zrmuo60d1p',
+
+    // A valid access token within the Space
+    accessToken: '21eeb92ae64b00197f221d0fde13ec467bfda6e57798cafab272a741cf251e55',
+
+    // Enable or disable SSL. Enabled by default.
+    secure: false,
+
+    // Resolve links to entries and assets
+    resolveLinks: true
+  });
+
+
 $(document).ready(function() {
+
+  // Get logos from contentful
+  client.getContentType({ content_type: 'educationLogos' })
+  .then(function (entries) {
+    populateLogoSection('education', entries);
+    // initializeFlipsForSection('education');
+  });
+  var educationLogos = [];
+
+  function populateLogoSection(sectionName, entries) {
+    var entry = entries[0];
+    var count = 0;
+
+    _.forEach(entry.fields.files, function(file) {
+      educationLogos.push({
+        file: file,
+        displaying: false,
+        element: null
+      });
+
+      if(count <=4) {
+        educationLogos[count].displaying = true;
+        educationLogos[count].element = '#' + sectionName + '_logo_' + count;
+        $('#' + sectionName + '_logo_' + count).attr('src', educationLogos[count].file.fields.file.url);
+      }
+
+      count++;
+
+      // set a timer for 6 seconds
+      setInterval(educationTimerTick, 6000);
+    });
+  }
+  console.log(client);
 
   $('a').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
@@ -16,6 +64,13 @@ $(document).ready(function() {
         }
     }
   });
+
+  $('#carouselEdu').carousel({
+    interval: 9000
+  });
+
+  // add more carousels
+
 
   $("#emailsubmit").click(function() {
     var email = $('#email');
